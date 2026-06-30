117185adbc81414d4746d7b0a24f060f755237a0
Ray <117043408+Snowlydial@users.noreply.github.com>
2026-06-30 10:49:15 +0300

    Added a .env for easier conf manip

--- web/frontend/src/api/api.js
+++ web/frontend/src/api/api.js
@@ -1,4 +1,4 @@
-const BASE = 'http://localhost:5000/api'
+const BASE = import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
 
 export async function generatePlaylist(criteria) {
   const res = await fetch(`${BASE}/playlists/generate`, {
